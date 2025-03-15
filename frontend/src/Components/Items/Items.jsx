e5f503424e20e85ff29b036ab5ee0df38254c26e
Navin-VijayaKumar <navinv10122004@gmail.com>
2025-03-15 21:40:46 +0530

    Skeleton Loaders

--- frontend/src/Components/Items/Items.jsx
+++ frontend/src/Components/Items/Items.jsx
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from "react";
-import "./Items.css";
+import React from "react";
 import { Link } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import "./Items.css";
 
-export const Items = (props) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
-
+export const Items = ({ id, image, name, new_price, old_price, loading }) => {
   return (
     <div className="container1">
       <div className="item">
         {loading ? (
-          <div className="skeleton-wrapper">
-            <div className="skeleton skeleton-img"></div>
-            <div className="skeleton skeleton-text"></div>
-            <div className="skeleton skeleton-price"></div>
-          </div>
+          // Skeleton Animation Structure
+          <>
+            <Skeleton height={350} width={375} />
+            <Skeleton height={30} width={200} style={{ margin: "10px 20px" }} />
+            <div className="price">
+              <Skeleton height={25} width={80} />
+              <Skeleton height={25} width={80} />
+            </div>
+          </>
         ) : (
-          <Link to={`/product/${props.id}`}>
-            <img onClick={() => window.scrollTo(0, 0)} src={props.image} alt="" />
+          // Actual Product Data
+          <Link to={`/product/${id}`}>
+            <img onClick={() => window.scrollTo(0, 0)} src={image} alt="" />
             <div>
-              <p>{props.name}</p>
+              <p>{name}</p>
             </div>
             <div className="price">
-              <div className="price-new">₹{props.new_price}</div>
-              <div className="price-old">₹{props.old_price}</div>
+              <div className="price-new">₹{new_price}</div>
+              <div className="price-old">₹{old_price}</div>
             </div>
           </Link>
         )}
